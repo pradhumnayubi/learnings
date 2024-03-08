49eb0a45b0e237a785c4f74e6e7323573b7902bf
pradhumnayubi <pradhumnasingh.rajput@go-yubi.com>
2024-03-08 11:46:40 +0530

    learned PQ

---
+++ src/march_8/priorityQueue.java
@@ -0,0 +1,21 @@
+package march_8;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class priorityQueue {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter list size: ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            pq.add(sc.nextInt());
+        }
+
+
+
+        System.out.println(pq);
+    }
+}
